1032abd2b01ad8a791215d016f6e20c8a044d715
Naomi Lagat <naomiejerutoe@gmail.com>
2024-04-08 11:39:55 +0300

    feat: Implement association proxies for Restaurant and Pizza models

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData, CheckConstraint
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,6 +12,8 @@ db = SQLAlchemy(metadata=metadata)
 class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurants'
     
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     address = db.Column(db.String, nullable=False)
@@ -19,11 +22,17 @@ class Restaurant(db.Model, SerializerMixin):
     restaurant_pizzas = db.relationship(
             'RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
     
+    # Association proxy to get pizzas for this restaurant through restaurant_pizzas
+    pizzas = association_proxy('restaurant_pizzas', 'pizza',
+            creator=lambda pizza_obj: RestaurantPizza(pizza=pizza_obj))
+    
     def __repr__(self):
         return f'<Restaurant {self.id}, {self.name}, {self.address}>'
 
 class Pizza(db.Model, SerializerMixin):
     __tablename__ = 'pizzas'
+    
+    serialize_rules = ('-restaurant_pizzas.pizza',)
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -33,11 +42,17 @@ class Pizza(db.Model, SerializerMixin):
     restaurant_pizzas = db.relationship(
             'RestaurantPizza', back_populates='pizza', cascade='all, delete-orphan')
     
+    # Association proxy to get restaurant for this pizza through restaurant_pizzas
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant',
+            creator=lambda restaurant_obj: RestaurantPizza(restaurant=restaurant_obj))
+    
     def __repr__(self):
         return f'<Pizza {self.id}, {self.name}, {self.ingredients}>'
 
 class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = 'restaurant_pizzas'
+    
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas',)
     
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float, nullable=False)
